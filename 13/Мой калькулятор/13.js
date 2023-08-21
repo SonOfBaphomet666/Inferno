463b69625364c79f8806c4d11488fb57537aa14c
SonOfBaphomet666 <Burning666@list.ru>
2023-08-21 13:24:10 +0700

    13
    Study

---
+++ 13/Мой калькулятор/13.js
@@ -0,0 +1,102 @@
+
+
+let a = '';
+let b = '';
+let sign = '';
+let finish = false;
+
+const digit= ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', 'X', '/', '+/-', '%'];
+
+const out = document.querySelector('.calc-screen p');
+
+function clearAll() {
+    a = '';
+    b = '';
+    sign = '';
+    finish = false;
+    out.textContent = 0;
+}
+
+document.querySelector('.ac').onclick = clearAll;
+
+document.querySelector('.buttons').onclick = (event) => {
+    // Нажата не кнопка
+    if(!event.target.classList.contains('btn')) return;
+    // нажата кнопка clearAll
+    if(event.target.classList.contains('ac')) return;
+
+    out.textContent = '';
+
+    // +- описываем
+    if(event.target.classList.contains('plus minus')){
+     // если нажата клавишас классом +-, то выполняется условие
+    } else if (event.target.classList.contains('percent')){
+    }
+
+out.textContent = a;
+
+const key = event.target.textContent;
+    
+// 0-9
+    if(digit.includes(key)) {
+        if (b ==='' && sign === '') {
+            a += key;
+
+            out.textContent = a;
+        }
+        else if(a!=='' && b!== '' && finish){
+            b = key;
+            finish = false;
+            out.textContent = b;
+}
+else{
+    b += key;
+    out.textContent = b;
+}
+console.table(a, b, sign);
+return;
+    }
+
+    // + - / * % +-
+if (action.includes(key)) {
+    sign = key;
+    out.textContent = sign;
+    console.table(a, b, sign);
+    return;
+    }
+    // =
+    if (key ==='=') {
+        if (b === '') b = a;
+        switch (sign) {
+            case "+":
+            a = (+a) + (+b);
+            break;
+            case "-":
+                a = a - b;
+                break;
+                case "X":
+                    a = a * b;
+                    break;
+                    case "/":
+                        if (b === '0') {
+                            out.textContent = 'Ошибка';
+                            a = '';
+                            b = '';
+                            sign = '';
+                            return;
+                        }
+                        a = a / b;
+                        break;
+                        case "+/-":
+                            a = -1 * a;
+                            break;
+                            case "%":
+                                a = a / 100;
+                                break;
+        }
+        finish = true;
+        out.textContent = a;
+        console.table(a, b, sign);
+    }
+}
---
+++ 13/Светофор2/index.js
@@ -0,0 +1,13 @@
+
+
+
+
+const btnNode = document.querySelector('.js-btn');
+const trafficLightNode = document.querySelector('.js-traffic-light');
+
+initLight(trafficLightNode);
+
+
+btnNode.addEventListener('click', function() {
+    switchLight(trafficLightNode);
+});
