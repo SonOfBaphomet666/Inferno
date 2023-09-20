6917d951dda98c3670bf9b2f4d759aa8eaecdba7
SonOfBaphomet666 <Burning666@list.ru>
2023-09-20 12:59:29 +0700

    btn

--- 6666/server.js
+++ 6666/server.js
@@ -1,13 +1,8 @@
-const s = [5, 7, 2];
-delete s[0];
-delete s[1];
-delete s[2];
-s.push(2,5,7)
 
 
+num1 = 5
+num2 = 3
 
+Calculate(operator = "*", num1, num2)
 
-
-
-
- console.log(s)
+console.log(Calculate)
