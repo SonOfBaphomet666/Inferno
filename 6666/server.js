24ae1f16bca911599c99e5b7a69259124db4a886
SonOfBaphomet666 <Burning666@list.ru>
2023-09-24 20:33:51 +0700

    btn

--- 6666/server.js
+++ 6666/server.js
@@ -1,8 +1,24 @@
 
 
-num1 = 5
-num2 = 3
+// const even = [2,4,6,8,10];
+// const odd = [1,3,5,7,9]
+// const sum= even.map(function (num,idx){
+// return num + odd[idx];
 
-Calculate(operator = "*", num1, num2)
+// });
+// console.log(sum)
 
-console.log(Calculate)
+let age = 18;
+
+if (age <= 6) {
+    console.log("Anna is a little child");
+} else if (age >= 6 && age < 18) {
+    console.log("Anna goes to school");
+} else if (age === 18) 
+    console.log("Anna is going to study at University");
+// } else if (age > 18 && age < 22) {
+//     console.log("Anna studies at University");
+// } else {
+//     console.log("Anna works");
+// }
+
