2272b33971c32e7394332b4b2275a457555665aa
SonOfBaphomet666 <Burning666@list.ru>
2023-09-27 14:36:00 +0700

    style

--- 6666/server.js
+++ 6666/server.js
@@ -1,24 +1,28 @@
 
+// const even = [2,4,6,8,10];
+// const odd = [1,3,5,7,9]
+
+// var sum = even.map(function (num, idx) {
+//   return num + odd[idx];
+// })
+
+// console.log(sum)
+
 
 // const even = [2,4,6,8,10];
 // const odd = [1,3,5,7,9]
-// const sum= even.map(function (num,idx){
-// return num + odd[idx];
 
-// });
-// console.log(sum)
+// function sumTheArrays(naturalNumbers) {
+//   naturalNumbers.map(function(num,idx) {
+//     return num + odd[idx];
+//   })
+// };
 
-let age = 18;
+// Следующие переменные объявленны в глобальном scope
 
-if (age <= 6) {
-    console.log("Anna is a little child");
-} else if (age >= 6 && age < 18) {
-    console.log("Anna goes to school");
-} else if (age === 18) 
-    console.log("Anna is going to study at University");
-// } else if (age > 18 && age < 22) {
-//     console.log("Anna studies at University");
-// } else {
-//     console.log("Anna works");
-// }
+function eq() {
+  var arr = [ 'a', 'b', 'c', 'd' ]
+  return arr.reverse;
+}
 
+console.log(ar);
