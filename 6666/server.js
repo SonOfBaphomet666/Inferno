ab2d79c19b111cad632b405aceb96e9f9edde22a
Maxim <Burning666@list.ru>
2023-11-05 00:27:27 +0700

    си

--- 6666/server.js
+++ 6666/server.js
@@ -1,12 +1,30 @@
 
 
-function copySorted(arr) {
-    return arr.slice().sort();
+
+function decodeSecretMessage(codePoints) {
+    let decodedMessage = "";
+  
+    for (let i = 0; i < codePoints.length; i++) {
+      const codePoint = codePoints[i];
+      decodedMessage += String.fromCharCode(codePoint);
+    }
+  
+    return decodedMessage;
   }
+  process.stdin.setEncoding('utf8');
   
-  let arr = ["HTML", "JavaScript", "CSS"];
+  let input = '';
   
-  let sorted = copySorted(arr);
+  process.stdin.on('data', (data) => {
+    input += data;
+  });
   
-  alert( sorted );
-  alert( arr );
+  process.stdin.on('end', () => {
+    const codePoints = input.trim().split(/,\s*/).map(Number);
+    const decodedMessage = decodeSecretMessage(codePoints);
+    process.stdout.write(decodedMessage);
+  });
+  
+  const secretMessage = [72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100];
+  const decodedMessage = decodeSecretMessage(secretMessage);
+  console.log(decodedMessage); // Выведет: "Hello World"
