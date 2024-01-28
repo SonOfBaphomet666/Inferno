474f14a82ef37a09dc0a835b23e5ce7736daf100
Maxim <Burning666@list.ru>
2024-01-28 16:22:38 +0700

    to-do

--- To-do list/script.js
+++ To-do list/script.js
@@ -1,25 +1,53 @@
 const inputField = document.querySelector(".input-field textarea"),
-todoLists = document.querySelector(".todoLists"),
-pendingNum = document.querySelector(".pending-num"),
-clearButton = document.querySelector(".clear-button");
+    todoLists = document.querySelector(".todoLists"),
+    pendingNum = document.querySelector(".pending-num"),
+    clearButton = document.querySelector(".clear-button");
+
+
+function allTasks() {
+    let tasks = document.querySelectorAll(".pending")
+    pendingNum.textContent = tasks.length === 0 ? "no" : tasks.length;
+
+    let allLists = document.querySelectorAll(".list");
+    if (allLists.length > 0) {
+        todoLists.style.marginTop = "20px";
+        clearButton.style.pointerEvents = "auto";
+        return
+    }
+    todoLists.style.marginTop = "0px";
+    clearButton.style.pointerEvents = "none"
+}
 
 inputField.addEventListener("keyup", (e) => {
     let inputVal = inputField.value.trim();
-    
+
     if (e.key === "Enter" && inputVal.length > 0) {
-        let liTag = `<li class="todoLists__list" onclick="handleStatus(this)">
+        let liTag = `<li class="todoLists__list pending" onclick="handleStatus(this)">
         <input type="checkbox">
         <span class="todoLists__task">${inputVal}</span>
-        <i class="gg-trash"></i>
+        <i class="gg-trash" onclick="deleteTask(this)"></i>
     </li>`;
 
-    todoLists.insertAdjacentHTML("beforeend", liTag);
-    inputField.value = "";
+        todoLists.insertAdjacentHTML("beforeend", liTag);
+        inputField.value = "";
+        allTasks();
     }
 });
 
 function handleStatus(e) {
     const checkbox = e.querySelector("input");
     console.log(checkbox);
-    checkbox.checked = checkbox.checked ? false : true
+    checkbox.checked = checkbox.checked ? false : true;
+    e.classList.toggle("pending");
+    allTasks();
+};
+
+function deleteTask(e) {
+    e.parentElement.remove();
+    allTasks();
 }
+
+clearButton.addEventListener("click", () => {
+    todoLists.innerHTML = "";
+    allTasks();
+})
