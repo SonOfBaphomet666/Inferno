c87e92b742ea9500bd72bf917ae164a992dbe8ea
Maxim <Burning666@list.ru>
2024-03-17 20:15:24 +0700

    card

--- Aero/JS/server.js
+++ Aero/JS/server.js
@@ -1,4 +1,20 @@
+// var card = document.querySelector(".card");
 
+// card.addEventListener('mousemove', function (e) {
+
+//     var wh = window.innerHeight / 2,
+//         ww = window.innerWidth / 2;
+//     card.style.setProperty('--mouseX7', (e.clientX - ww) / 25);
+//     card.style.setProperty('--mouseY7', (e.clientY - wh) / 25);
+
+// });
+
+// card.addEventListener('mouseleave', function (e) {
+
+//     card.style.setProperty('--mouseX7', 0);
+//     card.style.setProperty('--mouseY7', 0);
+
+// });
 
 
 
