39169c40d686ec008eaeaf75d4c1d96a07f813c3
Maxim <Burning666@list.ru>
2024-03-21 10:13:00 +0700

    story__card
    Don't work box-shadow and gradient

--- Aero/JS/server.js
+++ Aero/JS/server.js
@@ -1,25 +1,8 @@
-// var card = document.querySelector(".card");
 
-// card.addEventListener('mousemove', function (e) {
-
-//     var wh = window.innerHeight / 2,
-//         ww = window.innerWidth / 2;
-//     card.style.setProperty('--mouseX7', (e.clientX - ww) / 25);
-//     card.style.setProperty('--mouseY7', (e.clientY - wh) / 25);
-
-// });
-
-// card.addEventListener('mouseleave', function (e) {
-
-//     card.style.setProperty('--mouseX7', 0);
-//     card.style.setProperty('--mouseY7', 0);
-
-// });
-
-
-let userName = "Brad";
-let place = "first";
-console.log(`${userName} took ${place} place in the game.`)
+// story-card
+document.querySelectorAll(".story-card__description").forEach(function(box) {
+    $clamp(box, {clam:6});
+});
 
 
 
